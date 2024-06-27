03e680b06e2a23fc21f9438105bd6d54b67cc461
Neider Fabian Walteros Pinto <31144689+NeFaWaltPint@users.noreply.github.com>
2024-06-27 08:25:25 -0500

    Delete environments directory

--- environments/environment.development.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  assets: 'https://pruebasassets.portaloas.udistrital.edu.co/',
-  CONFIGURACION_SERVICE:
-    'https://autenticacion.portaloas.udistrital.edu.co/apioas/configuracion_crud_api/v1/',
-  NOTIFICACION_SERVICE: 'wss://pruebasapi.portaloas.udistrital.edu.co:8116/ws',
-};
